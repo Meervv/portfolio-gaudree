4c2c199d31aab66a12ef7a8206637bae08f45bfa
Meervv <marvine.gaudree.pro@gmail.com>
2021-10-25 15:24:57 +0200

    [ADD] all controllers

---
+++ src/Controller/VeilleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VeilleController extends AbstractController
+{
+    /**
+     * @Route("/veille", name="veille")
+     */
+    public function index(): Response
+    {
+        return $this->render('veille/index.html.twig', [
+            'controller_name' => 'VeilleController',
+        ]);
+    }
+}
