d8a2c64adf913606a49f38f11a6eef49d06a4946
Meervv <marvine.gaudree.pro@gmail.com>
2021-11-01 15:45:07 +0100

    [ADD] page certif + contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,15 +6,40 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Form\ContactType;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Contact;
+
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $contact = new Contact();
+
+        $manager = $this->getDoctrine()->getManager();
+        
+        $form = $this->createFormBuilder($contact)
+                     ->add('nom')
+                     ->add('prenom')
+                     ->add('mail')
+                     ->add('message')
+                     ->getForm();
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($contact);
+            $manager->flush();
+            return $this->redirectToRoute('contact');
+        }
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'formContact' => $form->createView()
         ]);
     }
 }
